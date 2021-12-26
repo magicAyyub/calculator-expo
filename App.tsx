c9599cf9b372022e8b052b7449f7c4cfb6336768
florentroques <florentroques@users.noreply.github.com>
2021-12-26 20:54:28 +0100

    darkmode header bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Appearance, StatusBar } from 'react-native';
 
 import HomeScreen from './screens/HomeScreen';
 import VATPriceScreen from './screens/VATPriceScreen';
@@ -13,14 +14,38 @@ export type RootStackParamList = {
 };
 
 const Stack = createNativeStackNavigator();
+const colorScheme = Appearance.getColorScheme();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Accueil' }}/>
-        <Stack.Screen name="VATPrice" component={VATPriceScreen} options={{ title: 'Calcul de prix TTC' }}/>
-        <Stack.Screen name="SteelReinforcement" component={SteelReinforcementScreen} options={{ title: 'Calcul de ferraillage' }}/>
+      <StatusBar
+        barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
+      />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'black'
+          },
+          headerTintColor: colorScheme === 'dark' ? 'lightgrey' : 'black',
+          animation: 'none'
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Accueil' }}
+        />
+        <Stack.Screen
+          name="VATPrice"
+          component={VATPriceScreen}
+          options={{ title: 'Calcul de prix TTC' }}
+        />
+        <Stack.Screen
+          name="SteelReinforcement"
+          component={SteelReinforcementScreen}
+          options={{ title: 'Calcul de ferraillage' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
