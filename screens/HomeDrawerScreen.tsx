680e6cd0e5288ce56cf83f706b2457383d0b6d47
florentroques <florentroques@users.noreply.github.com>
2021-12-28 12:55:54 +0100

    add custom drawer header

--- screens/HomeDrawerScreen.tsx
+++ screens/HomeDrawerScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Pressable, Text, StyleSheet, Appearance } from 'react-native';
+import { View, Pressable, Text, StyleSheet, Appearance, Button, Platform } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HomeScreen from './HomeScreen';
 import DataEntriesScreen from './DataEntriesScreen';
+import DrawerToggleButton from '../components/DrawerToggleButton';
 
 const Drawer = createDrawerNavigator();
 const colorScheme = Appearance.getColorScheme();
@@ -20,6 +21,21 @@ function HomeDrawerScreen() {
           shadowColor: 'transparent'
         },
         headerTintColor: colorScheme === 'dark' ? 'lightgrey' : 'black',
+        headerTitle: props => (
+          <Text
+            style={{
+              ...styles.title,
+              color: colorScheme === 'dark' ? 'white' : 'black',
+            }}
+          >
+            {props.children}
+          </Text>
+        ),
+        headerLeft: () => (
+          <DrawerToggleButton
+            tintColor={colorScheme === 'dark' ? 'white' : 'black'}
+          />
+        ),
       }}
     >
       <Drawer.Screen
@@ -30,10 +46,30 @@ function HomeDrawerScreen() {
       <Drawer.Screen
         name="DataEntries"
         component={DataEntriesScreen}
-        options={{ title: "Données d'entrée" }}
+        options={{
+          title: "Données d'entrée"
+        }}
       />
     </Drawer.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  title: Platform.select({
+    ios: {
+      fontSize: 17,
+      fontWeight: '600',
+    },
+    android: {
+      fontSize: 20,
+      fontFamily: 'sans-serif-medium',
+      fontWeight: 'normal',
+    },
+    default: {
+      fontSize: 18,
+      fontWeight: '500',
+    },
+  }),
+});
 
 export default HomeDrawerScreen;
