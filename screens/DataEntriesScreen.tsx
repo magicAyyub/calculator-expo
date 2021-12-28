6109406041783a33fd79ad62b2277cc26e7fa59f
florentroques <florentroques@users.noreply.github.com>
2021-12-28 12:05:36 +0100

    add data entries screen

--- screens/DataEntriesScreen.tsx
+++ screens/DataEntriesScreen.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
-import { View, Text, StyleSheet, Appearance } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Appearance, TextInput, Keyboard } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
 function DataEntriesScreen() {
-    return (
-      <View style={styles.container}>
-        <Text>Test</Text>
+  const [steelPrice, setSteelPrice] = useState('');
+
+  useEffect(() => {
+    async function setStoredSteelPrice() {
+      let storedSteelPrice = await SecureStore.getItemAsync('steelPrice');
+
+      if (storedSteelPrice) {
+        setSteelPrice(storedSteelPrice);
+      }
+    }
+
+    setStoredSteelPrice();
+  }, []);
+
+  useEffect(() => {
+    async function storeSteelPrice() {
+      await SecureStore.setItemAsync('steelPrice', steelPrice);
+    }
+
+    storeSteelPrice();
+  }, [steelPrice]);
+
+  function handleUnhandledTouches(){
+    Keyboard.dismiss();
+    return false;
+  }
+
+  return (
+    <View
+      style={styles.container}
+      onStartShouldSetResponder={handleUnhandledTouches}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+        }}
+      >
+        <Text style={{
+          ...styles.textColor,
+          ...styles.formLeftText,
+          ...styles.formTwoLinesText
+        }}>Prix de l'acier à la tonne</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          onChangeText={setSteelPrice}
+          value={steelPrice}
+          placeholder="Prix de l'acier à la tonne"
+          placeholderTextColor={colorScheme === 'dark' ? 'grey' : 'lightgrey'}
+        />
       </View>
-    );
+    </View>
+  );
 }
 
 const colorScheme = Appearance.getColorScheme();
@@ -15,9 +64,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: colorScheme === 'dark' ? '#18191a' : 'white',
-  }
+    paddingTop: 20
+  },
+  textColor: {
+    color: colorScheme === 'dark' ? 'lightgrey' : 'black',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 180,
+    borderColor: colorScheme === 'dark' ? 'grey' : 'black',
+    color: colorScheme === 'dark' ? 'lightgrey' : 'black'
+  },
+  formLeftText: {
+    width: 90,
+    textAlign: 'right'
+  },
+  formTwoLinesText: {
+    marginTop: 13
+  },
 });
 
 export default DataEntriesScreen;
